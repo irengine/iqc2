b2631a372b43a44861c43ece9f6b9a1c3487f4af
Alex Tang <alex.tang@taoware.com>
2013-05-04 14:13:39 +0800

    refactory application

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
 
-int main(int argc, char *argv[])
+QCoreApplication* createApplication(int &argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    
-    return a.exec();
+    for (int i = 1; i < argc; ++i)
+        if (!qstrcmp(argv[i], "-debug"))
+            return new QCoreApplication(argc, argv);
+    return new QApplication(argc, argv);
 }
+
+int main(int argc, char* argv[])
+{
+    QScopedPointer<QCoreApplication> app(createApplication(argc, argv));
+
+    if (qobject_cast<QApplication *>(app.data())) {
+        qDebug() << "release version.";
+
+        // entry point here
+        MainWindow w;
+        w.show();
+
+        return app->exec();
+    } else {
+        qDebug() << "debug version.";
+        return 0;
+    }
+}
+
